0aff265b39e37ec8016fa5f2426816d921026865
gavin stahl <stahlgavin@gmail.com>
2019-12-03 14:22:22 -0800

    added more than 10 events

--- js/index.js
+++ js/index.js
@@ -1,29 +1,33 @@
 // Your code goes here
 
-// 1
+//
 const busImg = document.querySelector("img");
 
 busImg.addEventListener("mouseenter", () => {
     busImg.style.transform = "scale(.9)";
     busImg.style.transition = "all 0.5s";
+    busImg.style.border = "2px solid black";
 })
 busImg.addEventListener("mouseleave", () => {
     busImg.style.transform = "scale(1)"
+    busImg.style.border = "0";
 });
 
-// 2
+//
 
 const midImages = document.querySelectorAll(".img-content > img").forEach(el => {
     el.addEventListener("mouseenter", () => {
         el.style.transform = "scale(1.1)";
         el.style.transition = "all 0.5s";
+        el.style.border = "2px solid black";
     })
     el.addEventListener("mouseleave", () => {
         el.style.transform = "scale(1)"
+        el.style.border = "0";
     });
 })
 
-// 3
+//
 
 const destinationImg = document.querySelector(".content-destination > img");
 
@@ -37,26 +41,70 @@ destinationImg.addEventListener("mouseleave", () => {
     destinationImg.style.border = "none"
 });
 
-// 4
+//
 
 const changeLogo = document.querySelector(".logo-heading");
 changeLogo.addEventListener("click", () => {
     changeLogo.style.color = 'violet';
 });
 
-// 5
+//
 
 const changeNav = document.querySelector(".main-navigation");
 changeNav.addEventListener("click", () => {
     changeNav.style.backgroundColor = 'lightgray';
 });
 
-// 6
+//
 
 const footer = document.querySelector(".footer");
 footer.addEventListener("dblclick", () => {
     footer.style.backgroundColor = 'lightgray';
 });
 
-// 7
+//
+
+document.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+    event.target.style.opacity = "";
+}, false);
+
+//
+
+function zoom(event) {
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    el.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const el = document.querySelector('.nav')
+el.onwheel = zoom;
+
+//
+
+let textHover = document.querySelectorAll('.destination > p').forEach(el => {
+    el.addEventListener("mouseover", function () {
+        el.style.color = "lightblue";
+        setTimeout(() => {
+            el.style.color = "";
+        }, 1000);
+    }, false);
+})
+
+//
+
+let linkHover = document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener("mouseover", function () {
+        el.style.color = "blue";
+    }, false);
+})
+
+//
 
